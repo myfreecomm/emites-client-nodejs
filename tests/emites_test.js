2e04598a8e45227452caa90e2d029a754c7a4a43
Luís Fernando Nascimento <luis.nascimento@nexaas.com>
2019-01-15 19:07:20 -0200

    Handling response with error

--- tests/emites_test.js
+++ tests/emites_test.js
@@ -8,46 +8,76 @@ describe('Emites', () => {
   const HOST = 'https://emites-sandbox.herokuapp.com';
   const organizationId = 11;
   const nfceId = 555;
+  const invalidNFCeId = 666;
   let emites = null;
 
   describe('GET #getNFCe', () => {
+    before(() => {
+      emites = new Emites({ host: HOST, accessToken: 'foo' });
+    });
+
     context('when it succeed', () => {
+      const contents = fs.readFileSync('tests/support/nfce_mocked_response.json');
+      const jsonContent = JSON.parse(contents);
+
       before(() => {
-        emites = new Emites({ host: HOST, accessToken: 'foo' });
-
-        const contents = fs.readFileSync('tests/support/nfce_mocked_response.json');
-        const jsonContent = JSON.parse(contents);
-
         nock(HOST)
-          .get(`/api/v1/organizations/${organizationId}/nfe/${nfceId}`)
+          .get(`/api/v1/organizations/${organizationId}/nfce/${nfceId}`)
           .reply(200, jsonContent);
       });
 
-      it('should respond with success status', async () => {
+      it('should return a NFCe json', async () => {
         const result = await emites.getNFCe(organizationId, nfceId);
-        assert.equal(result.status, 200);
-        assert.equal(result.data.nfce.status, 'succeeded');
+        assert.equal(result.nfce.status, 'succeeded');
+      });
+    });
+
+    context('when NFCe not found', () => {
+      before(() => {
+        nock(HOST)
+          .get(`/api/v1/organizations/${organizationId}/nfce/${invalidNFCeId}`)
+          .reply(404);
+      });
+
+      it('should respond with not found status (404)', async () => {
+        const result = await emites.getNFCe(organizationId, invalidNFCeId);
+        assert.equal(result, '');
       });
     });
   });
 
   describe('POST #createNFCeBatchNFCe', () => {
     context('when it succeed', () => {
+      const contents = fs.readFileSync('tests/support/nfce_batch_created_mocked_response.json');
+      const jsonContent = JSON.parse(contents);
+
       before(() => {
-        emites = new Emites({ host: HOST, accessToken: 'foo' });
-
-        const contents = fs.readFileSync('tests/support/nfce_batch_created_mocked_response.json');
-        const jsonContent = JSON.parse(contents);
-
         nock(HOST)
           .post(`/api/v1/organizations/${organizationId}/nfce_batch`, nfceParams.NFCE_BATCH_PARAMS)
           .reply(201, jsonContent);
       });
 
-      it('should respond with created status', async () => {
+      it('should create a NFCe and respond with its status', async () => {
         const result = await emites.createNFCeBatch(organizationId, nfceParams.NFCE_BATCH_PARAMS);
-        assert.equal(result.status, 201);
-        assert.equal(result.data.nfce_batch.nfces[0].status, 'processing');
+        assert.equal(result.nfce_batch.nfces[0].status, 'processing');
+      });
+    });
+
+    context('when it fails (invalid serie)', () => {
+      const params = nfceParams.NFCE_BATCH_PARAMS;
+      params.nfce_batch.serie = '666';
+      const contents = fs.readFileSync('tests/support/nfce_batch_create_fails_mocked_response.json');
+      const jsonContent = JSON.parse(contents);
+
+      before(() => {
+        nock(HOST)
+          .post(`/api/v1/organizations/${organizationId}/nfce_batch`, params)
+          .reply(422, jsonContent);
+      });
+
+      it('should not create a NFCe and respond with error message', async () => {
+        const result = await emites.createNFCeBatch(organizationId, params);
+        assert.deepEqual(result, jsonContent);
       });
     });
   });
